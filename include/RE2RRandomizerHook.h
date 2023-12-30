a0ce1afc6ebf482f398836ef6b3fb1402ef6809f
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2023-12-29 23:50:42 -0600

    Updated to add vcpkg minhook.

---
+++ include/RE2RRandomizerHook.h
@@ -0,0 +1,18 @@
+#ifndef RE2RRANDOMIZERHOOK_H
+#define RE2RRANDOMIZERHOOK_H
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#ifndef WIN32_LEAD_AND_MEAN
+#define WIN32_LEAD_AND_MEAN
+#endif
+
+#define RE2RRANDOMIZERHOOKAPI __declspec(dllexport)
+
+#endif
---
+++ src/RE2RRandomizerHook.cpp
@@ -0,0 +1,1 @@
+#include "RE2RRandomizerHook.h"
