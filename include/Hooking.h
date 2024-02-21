4d0c67f2d8eb64c595f37009fa5ce3e2691c073a
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2024-02-21 00:07:49 -0600

    Whatever 2.

--- include/Hooking.h
+++ include/Hooking.h
@@ -3,6 +3,13 @@
 
 #include <MinHook.h>
 
+/// @brief Attempts to hook the target function, calling the hook function in its place.
+/// @tparam FuncT The typedef prototype of the function to hook.
+/// @param target The target function.
+/// @param hook The hook function.
+/// @param original A reference to the original function.
+/// @param status The status code for the hook.
+/// @return A boolean indicating whether we succeeded in creating and enabling the hook.
 template <typename FuncT>
 bool TryHookFunction(FuncT target, FuncT hook, FuncT *original, MH_STATUS &status)
 {
